7c2bc0bd997f9702e1779d933ddcee98d802408e
aplki <aplki@protonmail.com>
2022-02-01 05:40:47 +0800

    v0 done, v1 WIP

---
+++ scripts/constants/index.ts
@@ -0,0 +1,6 @@
+export const V0_START_BLOCK_NUMBER = 12765981
+
+export const BPool: { xsgdusdc: string; thkdusdc: string } = {
+  xsgdusdc: '0xd6fe343d66c797c8c392d59768b86ffa2cd8d602',
+  thkdusdc: '0x309411c77CF68D5662c0D4dF68FB60f7E2dF3B65'
+}
---
+++ scripts/protocol-statistics/fetch-v1-stats.ts
@@ -0,0 +1,135 @@
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers'
+import { formatEther, formatUnits, parseUnits } from 'ethers/lib/utils'
+import { ExportToCsv } from 'export-to-csv'
+import { ethers } from 'hardhat'
+import { resolve } from 'path'
+import { BPool, V0_START_BLOCK_NUMBER } from '../constants'
+import { bptABI } from '../constants/abi/bpt'
+import * as fs from 'fs'
+
+// TODO: Transform to hardhat tasks
+
+interface v1Stats {
+  amountIn: string
+  amountOut: string
+  feesIn: string
+  feesOut: string
+  caller: string
+}
+
+const fetchV0Stats = async () => {
+  const [deployer] = await ethers.getSigners()
+  const options = {
+    fieldSeparator: ',',
+    quoteStrings: '"',
+    decimalSeparator: '.',
+    showLabels: true,
+    showTitle: true,
+    title: 'v1 Protocol Statistics',
+    useTextFile: false,
+    useBom: true,
+    useKeysAsHeaders: true
+  }
+  const csvExporter = new ExportToCsv(options)
+  let xsgdTotalAmountIn = 0
+  let xsgdTotalAmountOut = 0
+
+  const xsgdProtocolStats: v1stats[] = []
+
+  const xsgdusdc = new ethers.Contract(BPool['xsgdusdc'], bptABI, deployer)
+
+  const xsgdusdcEventFilter = await xsgdusdc.filters.LOG_SWAP()
+
+  const xsgdusdcSwapFee = formatEther(await xsgdusdc.getSwapFee())
+  const thkdusdcSwapFee = formatEther(await thkdusdc.getSwapFee())
+
+  const xsgdusdcEvents = await xsgdusdc.queryFilter(
+    xsgdusdcEventFilter,
+    V0_START_BLOCK_NUMBER,
+    14115866
+  )
+
+  const thkdusdcEvents = await thkdusdc.queryFilter(
+    thkdusdcEventFilter,
+    V0_START_BLOCK_NUMBER,
+    14115866
+  )
+
+  const xsgdusdcEventsArray = xsgdusdcEvents
+  const thkdusdcEventsArray = thkdusdcEvents
+
+  xsgdusdcEventsArray.forEach(log => {
+    xsgdTotalAmountIn += Number(log.args?.tokenAmountIn)
+    xsgdTotalAmountOut += Number(log.args?.tokenAmountOut)
+
+    xsgdProtocolStats.push({
+      amountIn: formatUnits(log.args?.tokenAmountIn, 6),
+      amountOut: formatUnits(log.args?.tokenAmountOut, 6),
+      feesIn: `${
+        Number(formatUnits(log.args?.tokenAmountIn, 6)) *
+        Number(xsgdusdcSwapFee)
+      }`,
+      feesOut: `${
+        Number(formatUnits(log.args?.tokenAmountIn, 6)) *
+        Number(xsgdusdcSwapFee)
+      }`,
+      caller: log.args?.caller
+    })
+  })
+
+  thkdusdcEventsArray.forEach(log => {
+    thkdTotalAmountIn += Number(log.args?.tokenAmountIn)
+    thkdTotalAmountOut += Number(log.args?.tokenAmountOut)
+
+    thkdProtocolStats.push({
+      amountIn: formatUnits(log.args?.tokenAmountIn, 6),
+      amountOut: formatUnits(log.args?.tokenAmountOut, 6),
+      feesIn: `${
+        Number(formatUnits(log.args?.tokenAmountIn, 6)) *
+        Number(thkdusdcSwapFee)
+      }`,
+      feesOut: `${
+        Number(formatUnits(log.args?.tokenAmountIn, 6)) *
+        Number(thkdusdcSwapFee)
+      }`,
+      caller: log.args?.caller
+    })
+  })
+
+  console.log(
+    `XSGD : { 
+    totalTokenAmountIn: ${formatUnits(xsgdTotalAmountIn, 6)}, 
+    totalAmountOut: ${formatUnits(xsgdTotalAmountOut, 6)} ,
+    totalFeesIn : ${
+      Number(formatUnits(xsgdTotalAmountIn, 6)) * Number(xsgdusdcSwapFee)
+    },
+    totalFeesOut : ${
+      Number(formatUnits(xsgdTotalAmountOut, 6)) * Number(xsgdusdcSwapFee)
+    }
+    }, 
+    THKD : {
+      totalTokenAmountIn: ${formatUnits(thkdTotalAmountIn, 18)}, 
+      totalAmountOut: ${formatUnits(xsgdTotalAmountIn, 18)},
+      totalFeesIn : ${
+        Number(formatUnits(thkdTotalAmountIn, 18)) * Number(thkdusdcSwapFee)
+      },
+      totalFeesOut : ${
+        Number(formatUnits(thkdTotalAmountOut, 18)) * Number(thkdusdcSwapFee)
+      }
+    }`
+  )
+
+  const xsgdStats = csvExporter.generateCsv(xsgdProtocolStats, true)
+  //const thkd = csvExporter.generateCsv(thkdProtocolStats, true)
+  fs.writeFileSync('v0ProtocolXSGD.csv', xsgdStats)
+  // fs.writeFileSync('v0ProtocolTHKD.csv', thkd)
+
+  console.log(xsgdProtocolStats)
+}
+
+fetchV0Stats()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  })
