74fa4ff804e8502c048c7d0e9c6e061cb3aab330
James Russell Orola <jrmorola@gmail.com>
2023-05-02 09:50:31 +0800

    feat: (wip) daily computation of rewards

--- scripts/xsgd-rewards-v2.ts
+++ scripts/xsgd-rewards-v2.ts
@@ -3,12 +3,7 @@ import { fxPoolABI } from './constants/abi/fxpool'
 import { rewardsOnlyGaugeABI } from './constants/abi/rewards-only-gauge'
 import { ZERO_ADDRESS } from './constants'
 import { BigNumber } from 'ethers'
-import {
-  formatEther,
-  formatUnits,
-  parseEther,
-  parseUnits
-} from 'ethers/lib/utils'
+import { formatEther, parseEther } from 'ethers/lib/utils'
 import { getSGDRate } from './util/cmc'
 import { ExportToCsv } from 'export-to-csv'
 import * as fs from 'fs'
@@ -16,7 +11,6 @@ import * as fs from 'fs'
 const POOL_ADDRESS = '0x726E324c29a1e49309672b244bdC4Ff62A270407'
 const GAUGE_ADDRESS = '0x3aC845345fc2d51A3006Ed384055cD5ACde86441'
 const ONE_ETH = parseEther('1')
-const XSGD_DECIMALS = 6
 
 export const snapshotXSGDRewards = async (
   hre: HardhatRuntimeEnvironment,
@@ -57,7 +51,7 @@ export const snapshotXSGDRewards = async (
   console.log(`Possible LPs: ${possibleLPs.length}`)
 
   /**
-   * STEP 2: Find out average BPT balance of all LPs on this epoch
+   * STEP 2: Compute for daily average balance and rewards of each LP
    */
 
   // Step 2.1 Find out the block number for each EOD
@@ -87,15 +81,21 @@ export const snapshotXSGDRewards = async (
   )
   const blocks = (await Promise.all(jsonPromises)).map((r: any) => r.height)
 
-  // Step 2.2 Get BPT and staked BPT balance of LPs for each block
-  const getAllBPTBalancePromises: any[] = []
-  const getAllStakedBPTBalancePromises: any[] = []
+  // Step 2.2 Get average BPT balancer for each block & compute for rewards
+  const userRewards: {
+    [block: number]: {
+      address: string
+      balance: BigNumber
+      share: BigNumber
+      reward: BigNumber
+    }[]
+  } = {}
 
-  possibleLPs.map(lp => {
+  for (const block of blocks) {
     const getBPTBalancePromises: any[] = []
     const getStakedBPTBalancePromises: any[] = []
 
-    blocks.map(block => {
+    possibleLPs.map(lp => {
       getBPTBalancePromises.push(
         FXPoolContract.balanceOf(lp, {
           blockTag: block
@@ -108,142 +108,215 @@ export const snapshotXSGDRewards = async (
       )
     })
 
-    getAllBPTBalancePromises.push(Promise.all(getBPTBalancePromises))
-    getAllStakedBPTBalancePromises.push(
-      Promise.all(getStakedBPTBalancePromises)
-    )
-  })
+    const [blockBPTBalances, blockStakedBPTBalances, [blockLiquidity]] =
+      await Promise.all([
+        Promise.all(getBPTBalancePromises),
+        Promise.all(getStakedBPTBalancePromises),
+        FXPoolContract.liquidity({
+          blockTag: block
+        })
+      ])
 
-  const allBPTBalancePromises = await Promise.all(getAllBPTBalancePromises)
-  const allStakedBPTBalancePromises = await Promise.all(
-    getAllStakedBPTBalancePromises
-  )
+    console.log('============================================')
+    console.log('BLOCK:', block)
+    console.log('blockLiquidity:', formatEther(blockLiquidity))
+    const blockReward = (blockLiquidity as BigNumber).mul(3).div(100).div(365)
+    console.log('blockReward:', formatEther(blockReward))
 
-  // Step 2.3 Compute for average BPT/Staked BPT balance
-  const userBalances: {
-    address: string
-    totalBPT: BigNumber
-    totalStakedBPT: BigNumber
-    averageBPT: BigNumber
-  }[] = []
-  const epochLength = BigNumber.from(blocks.length)
-  let epochAverageBPT = BigNumber.from(0)
-
-  possibleLPs.forEach((lp, index) => {
-    let totalBPTBalance = BigNumber.from(0)
-    allBPTBalancePromises[index].forEach((b: BigNumber, j: number) => {
-      totalBPTBalance = totalBPTBalance.add(b)
+    let blockTotalBPT = BigNumber.from(0)
+    possibleLPs.forEach((lp, index) => {
+      blockTotalBPT = blockTotalBPT
+        .add(blockBPTBalances[index])
+        .add(blockStakedBPTBalances[index])
     })
+    console.log('blockTotalBPT:', formatEther(blockTotalBPT))
 
-    let totalStakedBPTBalance = BigNumber.from(0)
-    allStakedBPTBalancePromises[index].forEach((b: BigNumber, j: number) => {
-      totalStakedBPTBalance = totalStakedBPTBalance.add(b)
+    userRewards[block] = []
+
+    possibleLPs.forEach((lp, index) => {
+      const userBPT = (blockBPTBalances[index] as BigNumber).add(
+        blockStakedBPTBalances[index] as BigNumber
+      )
+
+      if (userBPT.gt(0)) {
+        const share = userBPT.mul(ONE_ETH).div(blockTotalBPT)
+        const reward = blockReward.mul(share).div(ONE_ETH)
+
+        console.log(
+          `${lp} - ` +
+            `${Number(formatEther(userBPT)).toFixed(2)} BPT - ` +
+            `${(Number(formatEther(share)) * 100).toFixed(4)} % - ` +
+            `${Number(formatEther(reward)).toFixed(2)} $`
+        )
+
+        userRewards[block].push({
+          address: lp,
+          balance: userBPT,
+          share: share,
+          reward: reward
+        })
+      }
     })
-
-    const averageBPT = totalBPTBalance
-      .add(totalStakedBPTBalance)
-      .div(epochLength)
-    epochAverageBPT = epochAverageBPT.add(averageBPT)
-
-    userBalances.push({
-      address: lp,
-      totalBPT: totalBPTBalance,
-      totalStakedBPT: totalStakedBPTBalance,
-      averageBPT
-    })
-  })
-
-  /**
-   * STEP 3: Compute total amount of XSGD to be distributed this epoch
-   */
-  const [[tvl], totalSupply] = await Promise.all([
-    FXPoolContract.liquidity({
-      blockTag: blocks[blocks.length - 1]
-    }),
-    FXPoolContract.totalSupply({
-      blockTag: blocks[blocks.length - 1]
-    })
-  ])
-
-  console.log('epochAverageBPT: ', formatEther(epochAverageBPT))
-  const bptPrice = tvl.mul(ONE_ETH).div(totalSupply)
-  console.log('bptPrice: $', formatEther(bptPrice))
-  const epochAverageLiquidity = epochAverageBPT.mul(bptPrice).div(ONE_ETH)
-  console.log('epochAverageLiquidity: $', formatEther(epochAverageLiquidity))
-  const epochRewardAmount = epochAverageLiquidity.mul(3).div(100).div(12) // 3% APR
-  console.log(
-    'epochRewardAmount: $',
-    formatEther(epochRewardAmount),
-    '(3% APR)'
-  )
-  const sgdRate = await getSGDRate()
-  const epochRewardAmountXSGD = epochRewardAmount
-    .div(parseEther(`${sgdRate}`))
-    .mul(ONE_ETH)
-  console.log(
-    'epochRewardAmountXSGD: SG$',
-    formatEther(epochRewardAmountXSGD),
-    `(1 XSGD = ${sgdRate} USD)`
-  )
-
-  /**
-   * STEP 4: Compute XSGD reward for each LP
-   */
-  const userRewards: {
-    address: string
-    balance: BigNumber
-    share: number
-    rewards: BigNumber
-  }[] = []
-  userBalances.map(u => {
-    if (u.averageBPT.gt(0)) {
-      const share = u.averageBPT.mul(ONE_ETH).div(epochAverageBPT)
-      const rewards = epochRewardAmountXSGD.mul(share).div(ONE_ETH)
-      userRewards.push({
-        address: u.address,
-        balance: u.averageBPT,
-        share: Number(formatEther(share)),
-        rewards
-      })
-    }
-  })
-
-  const balanceFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'BPT'
-  })
-  const rewardFormatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'SGD'
-  })
-  const formattedUserRewards = userRewards.map(u => {
-    return {
-      address: u.address,
-      balance: balanceFormatter.format(Number(formatEther(u.balance))),
-      share: `${(u.share * 100).toFixed(4)}%`,
-      reward: rewardFormatter.format(Number(formatEther(u.rewards)))
-    }
-  })
-  console.table(formattedUserRewards)
-
-  /**
-   * STEP 5: Export result to csv
-   */
-  const options = {
-    fieldSeparator: ',',
-    quoteStrings: '"',
-    decimalSeparator: '.',
-    showLabels: true,
-    showTitle: true,
-    title: 'xsgd-rewards-snapshot-from-block',
-    useTextFile: false,
-    useBom: true,
-    useKeysAsHeaders: true
+    console.log('============================================')
   }
-  const csvExporter = new ExportToCsv(options)
-  const xsgdStats = csvExporter.generateCsv(formattedUserRewards, true)
-  fs.writeFileSync(
-    `xsgd-rewards-snapshot-from-block-${blocks[0]}.csv`,
-    xsgdStats
-  )
+
+  // // Step 2.2 Get BPT and staked BPT balance of LPs for each block
+  // const getAllBPTBalancePromises: any[] = []
+  // const getAllStakedBPTBalancePromises: any[] = []
+
+  // possibleLPs.map(lp => {
+  //   const getBPTBalancePromises: any[] = []
+  //   const getStakedBPTBalancePromises: any[] = []
+
+  //   blocks.map(block => {
+  //     getBPTBalancePromises.push(
+  //       FXPoolContract.balanceOf(lp, {
+  //         blockTag: block
+  //       })
+  //     )
+  //     getStakedBPTBalancePromises.push(
+  //       GaugeContract.balanceOf(lp, {
+  //         blockTag: block
+  //       })
+  //     )
+  //   })
+
+  //   getAllBPTBalancePromises.push(Promise.all(getBPTBalancePromises))
+  //   getAllStakedBPTBalancePromises.push(
+  //     Promise.all(getStakedBPTBalancePromises)
+  //   )
+  // })
+
+  // const allBPTBalancePromises = await Promise.all(getAllBPTBalancePromises)
+  // const allStakedBPTBalancePromises = await Promise.all(
+  //   getAllStakedBPTBalancePromises
+  // )
+
+  // // Step 2.3 Compute for average BPT/Staked BPT balance
+  // const userBalances: {
+  //   address: string
+  //   totalBPT: BigNumber
+  //   totalStakedBPT: BigNumber
+  //   averageBPT: BigNumber
+  // }[] = []
+  // const epochLength = BigNumber.from(blocks.length)
+  // let epochAverageBPT = BigNumber.from(0)
+
+  // possibleLPs.forEach((lp, index) => {
+  //   let totalBPTBalance = BigNumber.from(0)
+  //   allBPTBalancePromises[index].forEach((b: BigNumber, j: number) => {
+  //     totalBPTBalance = totalBPTBalance.add(b)
+  //   })
+
+  //   let totalStakedBPTBalance = BigNumber.from(0)
+  //   allStakedBPTBalancePromises[index].forEach((b: BigNumber, j: number) => {
+  //     totalStakedBPTBalance = totalStakedBPTBalance.add(b)
+  //   })
+
+  //   const averageBPT = totalBPTBalance
+  //     .add(totalStakedBPTBalance)
+  //     .div(epochLength)
+  //   epochAverageBPT = epochAverageBPT.add(averageBPT)
+
+  //   userBalances.push({
+  //     address: lp,
+  //     totalBPT: totalBPTBalance,
+  //     totalStakedBPT: totalStakedBPTBalance,
+  //     averageBPT
+  //   })
+  // })
+
+  // /**
+  //  * STEP 3: Compute total amount of XSGD to be distributed this epoch
+  //  */
+  // const [[tvl], totalSupply] = await Promise.all([
+  //   FXPoolContract.liquidity({
+  //     blockTag: blocks[blocks.length - 1]
+  //   }),
+  //   FXPoolContract.totalSupply({
+  //     blockTag: blocks[blocks.length - 1]
+  //   })
+  // ])
+
+  // console.log('epochAverageBPT: ', formatEther(epochAverageBPT))
+  // const bptPrice = tvl.mul(ONE_ETH).div(totalSupply)
+  // console.log('bptPrice: $', formatEther(bptPrice))
+  // const epochAverageLiquidity = epochAverageBPT.mul(bptPrice).div(ONE_ETH)
+  // console.log('epochAverageLiquidity: $', formatEther(epochAverageLiquidity))
+  // const epochRewardAmount = epochAverageLiquidity.mul(3).div(100).div(12) // 3% APR
+  // console.log(
+  //   'epochRewardAmount: $',
+  //   formatEther(epochRewardAmount),
+  //   '(3% APR)'
+  // )
+  // const sgdRate = await getSGDRate()
+  // const epochRewardAmountXSGD = epochRewardAmount
+  //   .div(parseEther(`${sgdRate}`))
+  //   .mul(ONE_ETH)
+  // console.log(
+  //   'epochRewardAmountXSGD: SG$',
+  //   formatEther(epochRewardAmountXSGD),
+  //   `(1 XSGD = ${sgdRate} USD)`
+  // )
+
+  // /**
+  //  * STEP 4: Compute XSGD reward for each LP
+  //  */
+  // const userRewards: {
+  //   address: string
+  //   balance: BigNumber
+  //   share: number
+  //   rewards: BigNumber
+  // }[] = []
+  // userBalances.map(u => {
+  //   if (u.averageBPT.gt(0)) {
+  //     const share = u.averageBPT.mul(ONE_ETH).div(epochAverageBPT)
+  //     const rewards = epochRewardAmountXSGD.mul(share).div(ONE_ETH)
+  //     userRewards.push({
+  //       address: u.address,
+  //       balance: u.averageBPT,
+  //       share: Number(formatEther(share)),
+  //       rewards
+  //     })
+  //   }
+  // })
+
+  // const balanceFormatter = new Intl.NumberFormat('en-US', {
+  //   style: 'currency',
+  //   currency: 'BPT'
+  // })
+  // const rewardFormatter = new Intl.NumberFormat('en-US', {
+  //   style: 'currency',
+  //   currency: 'SGD'
+  // })
+  // const formattedUserRewards = userRewards.map(u => {
+  //   return {
+  //     address: u.address,
+  //     balance: balanceFormatter.format(Number(formatEther(u.balance))),
+  //     share: `${(u.share * 100).toFixed(4)}%`,
+  //     reward: rewardFormatter.format(Number(formatEther(u.rewards)))
+  //   }
+  // })
+  // console.table(formattedUserRewards)
+
+  // /**
+  //  * STEP 5: Export result to csv
+  //  */
+  // const options = {
+  //   fieldSeparator: ',',
+  //   quoteStrings: '"',
+  //   decimalSeparator: '.',
+  //   showLabels: true,
+  //   showTitle: true,
+  //   title: 'xsgd-rewards-snapshot-from-block',
+  //   useTextFile: false,
+  //   useBom: true,
+  //   useKeysAsHeaders: true
+  // }
+  // const csvExporter = new ExportToCsv(options)
+  // const xsgdStats = csvExporter.generateCsv(formattedUserRewards, true)
+  // fs.writeFileSync(
+  //   `xsgd-rewards-snapshot-from-block-${blocks[0]}.csv`,
+  //   xsgdStats
+  // )
 }
